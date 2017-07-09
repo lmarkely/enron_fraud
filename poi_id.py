67154158b0581f6f65413e878f4e5b8da84b843e
Raga Markely <lmarkely@alum.mit.edu>
2017-07-08 22:24:24 -0400

    Run PCA and LDA

--- poi_id.py
+++ poi_id.py
@@ -10,7 +10,14 @@ from tester import dump_classifier_and_data
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi','salary'] # You will need to use more features
+### Include all quantitative features. In addition, 'std_from_poi' and
+### 'std_to_poi' are standardized feature (see details below).
+features_list = ['poi','salary', 'deferral_payments', 'total_payments',
+                 'loan_advances', 'bonus', 'restricted_stock_deferred',
+                 'deferred_income', 'total_stock_value', 'expenses',
+                 'exercised_stock_options', 'other', 'long_term_incentive',
+                 'restricted_stock', 'director_fees','shared_receipt_with_poi',
+                 'std_from_poi','std_to_poi']
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
@@ -25,6 +32,23 @@ print 'Total number of data points:',len(data_dict)
 ### Task 3: Create new feature(s)
 ### Store to my_dataset for easy export below.
 my_dataset = data_dict
+# Add new features: std_from_poi and std_to_poi by dividing the message
+# to/from poi by the total sent or received messages, respectively.
+for key in data_dict:
+    if (type(data_dict[key]['from_poi_to_this_person']) == int and
+        type(data_dict[key]['from_messages']) == int):
+        data_dict[key]['std_from_poi'] = \
+        (data_dict[key]['from_poi_to_this_person']/
+         data_dict[key]['from_messages'])
+    else:
+        data_dict[key]['std_from_poi'] = 0
+    if (type(data_dict[key]['from_this_person_to_poi']) == int and
+        type(data_dict[key]['to_messages']) == int):
+        data_dict[key]['std_to_poi'] = \
+        (data_dict[key]['from_this_person_to_poi']/
+         data_dict[key]['to_messages'])
+    else:
+        data_dict[key]['std_to_poi'] = 0
 
 ### Extract features and labels from dataset for local testing
 data = featureFormat(my_dataset, features_list, sort_keys = True)
@@ -65,7 +89,17 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
+# For simplicity, rename features as X and labels as y
+X = features
+y = labels
 
+X_std = StandardScaler().fit_transform(X)
+pca = PCA(n_components=2)
+X_pca = pca.fit_transform(X_std)
+lda = LinearDiscriminantAnalysis(n_components=2)
+X_lda = lda.fit_transform(X_std,y)
+print 'PCA explained_variance_ratio_', pca.explained_variance_ratio_
+print 'LDA explained variance:', lda.explained_variance_ratio_
 
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall
 ### using our testing script. Check the tester.py script in the final project
@@ -75,13 +109,13 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
 # Example starting point. Try investigating other evaluation techniques!
-from sklearn.cross_validation import train_test_split
-features_train, features_test, labels_train, labels_test = \
-    train_test_split(features, labels, test_size=0.3, random_state=42)
+#from sklearn.cross_validation import train_test_split
+#features_train, features_test, labels_train, labels_test = \
+#    train_test_split(features, labels, test_size=0.3, random_state=42)
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
 ### that the version of poi_id.py that you submit can be run on its own and
 ### generates the necessary .pkl files for validating your results.
 
-dump_classifier_and_data(clf, my_dataset, features_list)
+#dump_classifier_and_data(clf, my_dataset, features_list)
